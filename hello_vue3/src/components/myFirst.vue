eb36514c915ccc754c0b18a6366584652a53b012
NicoAndSteven <1215240348@qq.com>
2024-12-03 09:45:49 +0800

    all files to commit

---
+++ hello_vue3/src/components/myFirst.vue
@@ -0,0 +1,22 @@
+<template>
+<h1>this is it!</h1>
+<div :class="on">{{ student }}</div>
+</template>
+
+<script>
+export default{
+    data(){
+        student:"Steven"
+    }
+}
+
+</script>
+
+<style>
+    .on{
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+    }
+</style>
